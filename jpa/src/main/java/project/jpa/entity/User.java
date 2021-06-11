88ab4c422d09df007984f2b639d4224709326714
dirtysmile <dirtysmile89@naver.com>
2021-06-11 23:55:10 +0900

    엔티티 생성 마무리

--- jpa/src/main/java/project/jpa/entity/User.java
+++ jpa/src/main/java/project/jpa/entity/User.java
@@ -13,11 +13,14 @@ public class User extends BaseEntity{
     @Id @GeneratedValue
     @Column(name = "user_id")
     private Long id;
+    private String personal_id;
     private String password;
     private String name;
     private String phone;
     private String email;
     private LocalDate birthday;
+
+
 
     @Enumerated(value = EnumType.STRING)
     private Gender gender;
