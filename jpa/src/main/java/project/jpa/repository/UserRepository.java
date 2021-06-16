d7ebf6d205bbb26fda1b07680a197ea9add11944
dirtysmile <dirtysmile89@naver.com>
2021-06-16 23:18:52 +0900

    서비스 생성

--- jpa/src/main/java/project/jpa/repository/UserRepository.java
+++ jpa/src/main/java/project/jpa/repository/UserRepository.java
@@ -3,7 +3,9 @@ package project.jpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.jpa.entity.User;
 
+import java.util.List;
+
 public interface UserRepository  extends JpaRepository<User,Long> {
 
-
+    List<User> findByPersonal_id(String personal_id);
 }
--- jpa/src/main/java/project/jpa/service/UserService.java
+++ jpa/src/main/java/project/jpa/service/UserService.java
@@ -1,8 +1,26 @@
 package project.jpa.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.jpa.entity.User;
+import project.jpa.repository.UserRepository;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Long join(User user){
+        validateDublicatePersionalId(user);
+
+        return userRepository.save(user).getId();
+    }
+
+    private void validateDublicatePersionalId(User user) {
+        userRepository.fin
+    }
 
 }
