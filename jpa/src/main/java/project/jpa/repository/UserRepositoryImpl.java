f95b43fa3c316bf9ac742552bedb020e8572fd66
dirtysmile <dirtysmile89@naver.com>
2021-07-11 22:25:34 +0900

    queryDsl 적용중

--- jpa/src/main/java/project/jpa/repository/UserRepositoryImpl.java
+++ jpa/src/main/java/project/jpa/repository/UserRepositoryImpl.java
@@ -1,6 +1,8 @@
 package project.jpa.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.util.StringUtils;
 import project.jpa.dto.UserSearchCondition;
 import project.jpa.entity.QUser;
 import project.jpa.entity.User;
@@ -20,10 +22,29 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
 
     @Override
     public List<User> search(UserSearchCondition condition) {
+
         List<User> fetch = queryFactory
                 .selectFrom(user)
-                .where(user.name.eq("administor"))
+                .where(
+                        usernameEq(condition.getName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
                 .fetch();
-        return null;
+        return fetch;
+    }
+
+
+
+    private BooleanExpression usernameEq(String username){
+        return StringUtils.hasText(username) ? user.name.eq(username) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe !=null ? user.age.goe(ageGoe): null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe !=null ? user.age.loe(ageLoe): null;
     }
 }
